2d8a7eee3ab8772c5eab220d6d07778019ad7b94
SK8TERBOI04 <150426448+SK8TERBOI04@users.noreply.github.com>
2025-12-03 17:07:16 +0700

    Write File myfile.txt

---
+++ src/CreateFile.java
@@ -0,0 +1,18 @@
+import java.io.File;
+import java.io.IOException;
+
+public class CreateFile {
+    public static void main(String[] args) {
+        try {
+            File myfile = new File("myfile.txt");
+            if(myfile.createNewFile()) {
+                System.out.println("File created: " + myfile.getName());
+            } else {
+                System.out.println("File already exists.");
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+}
